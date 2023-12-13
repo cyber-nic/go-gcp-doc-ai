60b40c035039b1759ccfac04e856f166e9a9e3be
Nicolas Delorme <nic@4ks.io>
2023-12-13 09:32:48 +0000

    clean up test buckets

--- iac/main.tf
+++ iac/main.tf
@@ -7,33 +7,6 @@ resource "google_firestore_database" "database" {
   delete_protection_state = "DELETE_PROTECTION_ENABLED"
 }
 
-# test buckets
-
-# resource "google_storage_bucket" "src_checkpoint_test" {
-#   name          = "${var.project_prefix}-src-checkpoint-test"
-#   location      = local.region
-#   force_destroy = true
-# }
-
-# resource "google_storage_bucket" "ocr_refs_test" {
-#   name          = "${var.project_prefix}-ocr-refs-test"
-#   location      = local.region
-#   force_destroy = true
-# }
-
-# resource "google_storage_bucket" "ocr_err_test" {
-#   name          = "${var.project_prefix}-ocr-err-test"
-#   location      = local.region
-#   force_destroy = true
-# }
-
-# resource "google_storage_bucket" "ocr_data_test" {
-#   name          = "${var.project_prefix}-ocr-data-test"
-#   location      = local.region
-#   force_destroy = true
-# }
-
-
 # buckets
 
 // used by deduper
@@ -43,7 +16,6 @@ resource "google_storage_bucket" "src_checkpoint" {
   force_destroy = true
 }
 
-
 // used by dispatcher
 resource "google_storage_bucket" "dispatcher_checkpoint" {
   name          = "${var.project_prefix}-dispatcher-checkpoint"
@@ -51,23 +23,11 @@ resource "google_storage_bucket" "dispatcher_checkpoint" {
   force_destroy = true
 }
 
-# resource "google_storage_bucket" "dispatcher_checkpoint_test" {
-#   name          = "${var.project_prefix}-dispatcher-checkpoint-test"
-#   location      = local.region
-#   force_destroy = true
-# }
-
 resource "google_storage_bucket" "dispatcher_refs" {
   name          = "${var.project_prefix}-dispatcher-refs"
   location      = local.region
   force_destroy = true
 }
-
-# resource "google_storage_bucket" "dispatcher_refs_test" {
-#   name          = "${var.project_prefix}-dispatcher-refs-test"
-#   location      = local.region
-#   force_destroy = true
-# }
 
 // used by ocr-worker
 resource "google_storage_bucket" "ocr_err" {
@@ -95,20 +55,8 @@ resource "google_storage_bucket" "nlp_data" {
   force_destroy = true
 }
 
-# resource "google_storage_bucket" "nlp_data_test" {
-#   name          = "${var.project_prefix}-nlp-data-test"
-#   location      = local.region
-#   force_destroy = true
-# }
-
 resource "google_storage_bucket" "nlp_err" {
   name          = "${var.project_prefix}-nlp-err"
   location      = local.region
   force_destroy = true
 }
-
-# resource "google_storage_bucket" "nlp_err_test" {
-#   name          = "${var.project_prefix}-nlp-err-test"
-#   location      = local.region
-#   force_destroy = true
-# }
